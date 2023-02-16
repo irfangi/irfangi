7ea61a52e8a0fd965c23464ccef5f7453c83df82
Irfangi <irfangi464@gmail.com>
2023-02-16 17:38:28 +0700

    add firestore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxtjs/color-mode"],
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/css/main.css", "~/assets/css/font.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
