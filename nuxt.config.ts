2c016381adba8205cfa30efbaec4ab993747d196
Irfangi <irfangi464@gmail.com>
2023-07-05 00:26:17 +0700

    remove color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   components: [{ path: "~/components", extensions: [".vue"] }],
-  modules: ["@nuxt/content", "@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   css: ["@/assets/css/font.css"],
   postcss: {
     plugins: {
